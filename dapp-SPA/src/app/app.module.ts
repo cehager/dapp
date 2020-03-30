e23bbc1551d60b53aec1959216780aefd0e8ae4f
cehager <charlie80088@gmail.com>
2020-03-30 16:48:26 -0600

    added members detail page

--- dapp-SPA/src/app/app.module.ts
+++ dapp-SPA/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { JwtModule } from "@auth0/angular-jwt";
 
 import { AppComponent } from "./app.component";
 import { ValueComponent } from "./value/value.component";
@@ -12,10 +13,19 @@ import { HomeComponent } from "./home/home.component";
 import { RegisterComponent } from "./register/register.component";
 import { ErrorInterceptorProvider } from "./_services/error.interceptor";
 import { BsDropdownModule } from "ngx-bootstrap/dropdown";
-import { ConnectionListComponent } from "./connection-list/connection-list.component";
+import { TabsModule } from "ngx-bootstrap/tabs";
+import { ConnectionListComponent } from "./connections/connection-list/connection-list.component";
 import { MyListsComponent } from "./my-lists/my-lists.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { appRoutes } from "./routes";
+import { MemberCardComponent } from "./connections/connection-list/member-card/member-card.component";
+import { MemberDetailComponent } from "./connections/connection-list/member-detail/member-detail.component";
+import { MemberDetailResolver } from "./_resolvers/member-detail.resolver";
+import { MemberListResolver } from "./_resolvers/member-list.resolver";
+
+export function tokenGetter() {
+  return localStorage.getItem("token");
+}
 
 @NgModule({
   declarations: [
@@ -26,16 +36,31 @@ import { appRoutes } from "./routes";
     RegisterComponent,
     ConnectionListComponent,
     MyListsComponent,
-    MessagesComponent
+    MessagesComponent,
+    MemberCardComponent,
+    MemberDetailComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     BsDropdownModule.forRoot(),
-    RouterModule.forRoot(appRoutes)
+    TabsModule.forRoot(),
+    RouterModule.forRoot(appRoutes),
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: tokenGetter,
+        whitelistedDomains: ["localhost:5000"],
+        blacklistedRoutes: ["localhost:5000/api/auth"]
+      }
+    })
   ],
-  providers: [AuthService, ErrorInterceptorProvider],
+  providers: [
+    AuthService,
+    ErrorInterceptorProvider,
+    MemberDetailResolver,
+    MemberListResolver
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- dapp-SPA/src/app/routes.ts
+++ dapp-SPA/src/app/routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
-import { ConnectionListComponent } from "./connection-list/connection-list.component";
+import { ConnectionListComponent } from "./connections/connection-list/connection-list.component";
 import { MessagesComponent } from "./messages/messages.component";
 import { MyListsComponent } from "./my-lists/my-lists.component";
 import { AuthGuard } from "./_guards/auth.guard";
+import { MemberDetailComponent } from "./connections/connection-list/member-detail/member-detail.component";
+import { MemberDetailResolver } from "./_resolvers/member-detail.resolver";
+import { MemberListResolver } from "./_resolvers/member-list.resolver";
 
 export const appRoutes: Routes = [
   { path: "", component: HomeComponent },
@@ -12,7 +15,16 @@ export const appRoutes: Routes = [
     runGuardsAndResolvers: "always",
     canActivate: [AuthGuard],
     children: [
-      { path: "members", component: ConnectionListComponent },
+      {
+        path: "members",
+        component: ConnectionListComponent,
+        resolve: { users: MemberListResolver }
+      },
+      {
+        path: "members/:id",
+        component: MemberDetailComponent,
+        resolve: { user: MemberDetailResolver }
+      },
       { path: "messages", component: MessagesComponent },
       { path: "lists", component: MyListsComponent }
     ]
